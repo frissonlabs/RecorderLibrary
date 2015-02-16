9c207ce8723bcea4ef6bed16f3a5b21e229583b2
David Li <david@Davids-Mac.local>
2015-02-16 01:03:26 -0800

    Initial Commit

---
+++ Recorder/Recorder.h
@@ -0,0 +1,13 @@
+//
+//  Recorder.h
+//  Recorder
+//
+//  Created by David Li on 2/16/15.
+//  Copyright (c) 2015 Euphonia Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recorder : NSObject
+
+@end
---
+++ Recorder/Recorder.m
@@ -0,0 +1,13 @@
+//
+//  Recorder.m
+//  Recorder
+//
+//  Created by David Li on 2/16/15.
+//  Copyright (c) 2015 Euphonia Labs. All rights reserved.
+//
+
+#import "Recorder.h"
+
+@implementation Recorder
+
+@end
